65637694376ad393fd05cd5be2fb927e252992d1
Tristan <rechenberger@me.com>
2018-02-23 14:05:04 +0100

    initProject

--- test.js
+++ test.js
@@ -1,12 +1,26 @@
 const fs = require('fs')
 const rimraf = require('rimraf')
+const cli = require('@angular/cli')
+const { exec } = require('child_process')
+
+const PROJECTNAME = 'test-project'
 
 async function main() {
-  removeProject()
+  await removeProject()
+  await initProject()
 }
 
 async function removeProject() {
-  new Promise(res => rimraf('test-project', res))
+  new Promise(res => rimraf(PROJECTNAME, res))
+}
+
+async function initProject() {
+  new Promise((res, rej) => {
+    exec(`ng new ${PROJECTNAME}`, (err) => {
+      if(err) return rej(err)
+      return res()
+    })
+  })
 }
 
 
