8c532fae100adcfd516c01faf57b324ef8ef6db3
Tristan <rechenberger@me.com>
2018-02-23 14:30:48 +0100

    copy

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const fs = require('fs')
 const cpx = require('cpx')
+const changeCase = require('change-case')
+
+class NgRename {
+  constructor(oldName, newName, options) {
+    this.dir = options.dir || process.env.cwd
+    this.old = this.createNames(oldName)
+    this.new = this.createNames(newName)
+  }
+
+  execute() {
+    // Copy Files
+    this.copyFiles()
+    // Rename Files
+    // Replace EVERYWHERE
+    // (next char is not: -,[A-Z] )
+  }
+
+  async copyFiles() {
+    await new Promise((res, rej) => cpx.copy(`${this.old.folder}/**`, `${this.new.folder}`, err => err ? rej(err) : res()))
+  }
+
+  createNames(name) {
+    const names = {
+      fileName: changeCase.paramCase(name),
+      className: changeCase.pascalCase(name),
+
+    }
+    names.folder = `${this.dir}/${names.fileName}`
+    return names
+  }
+}
+
+module.exports = { NgRename }
--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 const fs = require('fs')
 const rimraf = require('rimraf')
-const cli = require('@angular/cli')
 const { exec } = require('child_process')
+const { NgRename } = require('.')
 
 const PROJECTNAME = 'test-project'
+const COMP_DIR = `${__dirname}/${PROJECTNAME}/src/app`
+const OLD_COMP_NAME = 'some-button'
+const NEW_COMP_NAME = 'better-button'
 
 async function main() {
-  await removeProject()
-  await initProject()
+  // await removeProject()
+  // await initProject()
+  await newComponent()
+  await rename()
 }
 
 async function removeProject() {
@@ -15,12 +20,27 @@ async function removeProject() {
 }
 
 async function initProject() {
+  return executeCommand(`ng new ${PROJECTNAME}`)
+}
+
+async function newComponent() {
+  return executeCommand(`cd ${COMP_DIR} && ng g c ${OLD_COMP_NAME}`)
+}
+
+async function executeCommand(command) {
   new Promise((res, rej) => {
-    exec(`ng new ${PROJECTNAME}`, (err) => {
-      if(err) return rej(err)
+    exec(command, (err) => {
+      if (err) return rej(err)
       return res()
     })
   })
+}
+
+async function rename() {
+  const ngRename = new NgRename(OLD_COMP_NAME, NEW_COMP_NAME, {
+    dir: COMP_DIR
+  })
+  ngRename.execute()
 }
 
 
