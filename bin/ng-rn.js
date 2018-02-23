4f38aa3f8ace0d8c614ef8e8ccdefaba24ff0356
Tristan <rechenberger@me.com>
2018-02-23 16:03:52 +0100

    cwd fix

--- bin/ng-rn.js
+++ bin/ng-rn.js
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
+'use strict'
+process.title = 'ng-rn'
 const { NgRename } = require('../.')
 const [oldName, newName] = process.argv.slice(2)
-const dir = process.env.cwd
+const dir = process.cwd()
 const ngRename = new NgRename(oldName, newName, { dir })
 ngRename.execute()
