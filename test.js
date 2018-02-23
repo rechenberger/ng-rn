032576f633c3bb01e8b766d3df966a4edf270ce3
Tristan <rechenberger@me.com>
2018-02-23 13:56:24 +0100

    removeProject

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
 const fs = require('fs')
+const rimraf = require('rimraf')
 
+async function main() {
+  removeProject()
+}
+
+async function removeProject() {
+  new Promise(res => rimraf('test-project', res))
+}
+
+
+main()
